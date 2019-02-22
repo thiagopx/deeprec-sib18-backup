507ef5c855525fbbf3f635682851b8bb23f836aa
LCAD <lcad@lcad06.lcad.lan>
2019-02-21 23:15:02 -0300

    docker

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
-docker run --runtime=nvidia -v $PWD:/home/$USER/sib18 -it sib18:latest  sh -c "cd /home/$USER/sib18 && python train.py -e 5 -bs 128 -lr 0.0001 -s 0.33 -d datasets/patches"
+docker run --runtime=nvidia --user=$USER -v $PWD:/home/$USER/deeprec-sib18 -it deeprec-sib18:latest  sh -c ". envs/deeprec-sib18/bin/activate && cd /home/$USER/deeprec-sib18 && python train.py -e 5 -bs 128 -lr 0.0001 -s 0.33 -d datasets/patches"
 
 
 
